d59cebd7cdddde05555cfcc5881cc25f0e0485ab
Magnos Lima <magandroid6.1@gmail.com>
2025-12-02 20:16:43 -0300

    chore: add project configuration module and settings loader

---
+++ app/core/config.py
@@ -0,0 +1,8 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    llm_model: str = "qwen3:4b"
+    llm_endpoint: str = "http://localhost:11434"
+
+    class Config:
+        env_file = ".env"
